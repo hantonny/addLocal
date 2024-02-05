09a12a6190b0a652dc05c5e05d78796112865729
Hantonny Korrea <hantonny.korrea@nastek.com.br>
2024-02-05 10:53:26 -0400

    Selecionando Localização #01

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -4,11 +4,13 @@ import 'package:localiza/models/place.dart';
 
 class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
+  final bool isReadonly;
   const MapScreen(
       {this.initialLocation = const PlaceLocation(
         latitude: 37.419857,
         longitude: -122.078827,
       ),
+      this.isReadonly = false,
       super.key});
 
   @override
@@ -16,6 +18,14 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedPosition;
+
+  void _selectPosition(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +45,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 13,
         ),
+        onTap: widget.isReadonly ? null : _selectPosition,
+        markers: _pickedPosition == null
+            ? {}
+            : {
+                Marker(
+                  markerId: MarkerId('p1'),
+                  position: _pickedPosition!,
+                )
+              },
       ),
     );
   }
